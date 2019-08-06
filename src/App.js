a23935ad1df0142347a69c43050f939d29995c72
ankitshukla065 <ankitshukla065@gmail.com>
2019-08-07 00:28:31 +0530

    Fixed issues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import RouteProvider from './modules/RouteProvider';
+import './assets/css/main_styles.css';
+import './assets/css/responsive.css';
+import 'react-owl-carousel2/lib/styles.css';
+import './assets/css/owl.theme.default.scss';
+import './assets/css/animate.css';
 import './assets/css/index.scss';
 
 function App() {
   return (
-    <RouteProvider />
+      <RouteProvider />
   );
 }
 
