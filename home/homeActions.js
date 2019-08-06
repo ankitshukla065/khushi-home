a141b815704e8e0d6f0c4d07b9431c0fbfebef27
ankitshukla065 <ankitshukla065@gmail.com>
2019-08-06 23:28:59 +0530

    Revert "Home page changes"
    This reverts commit 4cbccb4f

--- home/homeActions.js
+++
@@ -1,1 +0,0 @@
-import {HomeConstants} from './homeConstants';
--- home/homeReducer.js
+++
@@ -1,6 +0,0 @@
-import {HomeConstants} from './homeConstants';
-import createReducer from '../../modules/createReducer';
-
-const initialState = {};
-
-export const homeReducer = createReducer(initialState, {});
