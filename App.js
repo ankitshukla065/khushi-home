4cbccb4f936b86af4bc0ad04d5389dd45ef20931
Ankit Shukla <ankitshukla065@gmail.com>
2019-08-06 22:45:53 +0530

    Home page changes

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import RouteProvider from './modules/RouteProvider';
+import './assets/css/main_styles.css';
+import './assets/css/responsive.css';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import './assets/css/animate.css';
+import './assets/css/index.scss';
+import 'owl.carousel/dist/owl.carousel';
+
+function App() {
+  return (
+    <RouteProvider />
+  );
+}
+
+export default App;
---
+++ home/homeReducer.js
@@ -0,0 +1,6 @@
+import {HomeConstants} from './homeConstants';
+import createReducer from '../../modules/createReducer';
+
+const initialState = {};
+
+export const homeReducer = createReducer(initialState, {});
